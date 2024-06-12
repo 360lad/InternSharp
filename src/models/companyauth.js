5dc6a0eb8bf633719e2d5b553d2857f46c3f61e8
L.A360 <lat.tif8605@gmail.com>
2024-06-12 16:20:42 +0300

    usertype was added on the user details

--- src/models/companyauth.js
+++ src/models/companyauth.js
@@ -3,6 +3,9 @@ const companySchema=mongoose.Schema({
     companyName:{type:String,required:true,unique:false},
     companyEmail:{type:String,required:true,unique:true},
     companyPassword:{type:String,required:true,unique:false},
-    companyWebsite:{type:String,required:false,unique:false}
+    companyWebsite:{type:String,required:false,unique:false},
+    userType:{type:String,required:false,unique:false}
+
+
 })
 export default mongoose.model("Company",companySchema)
