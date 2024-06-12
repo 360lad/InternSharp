749522bee2f7117c6c1cc59b26fb0e5a062b1c8c
L.A360 <lat.tif8605@gmail.com>
2024-06-12 10:47:16 +0300

    the Crud methods in the internships were added

--- src/routes/internship.js
+++ src/routes/internship.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/new",async (req, res) => {
+router.post("/new", async (req, res) => {
   try {
     const {
       internshipTitle,
@@ -13,7 +13,7 @@ router.post("/new",async (req, res) => {
       paid,
       companyId,
     } = req.body;
-    const newInternship =await new Internship({
+    const newInternship = await new Internship({
       internshipTitle,
       description,
       requirements,
@@ -21,23 +21,61 @@ router.post("/new",async (req, res) => {
       paid,
       companyId,
     });
-const savedInternship=await newInternship.save()
-return res.status(201).json(savedInternship)
-
+    const savedInternship = await newInternship.save();
+    return res.status(201).json(savedInternship);
   } catch (error) {
-    return res.status(500).json({message:error})
+    return res.status(500).json({ message: error });
   }
 });
 
 //Getting all internships
-router.get("/",async(req,res)=>{
-    try {
-         const allInternships=await Internship.find();
-          return res.status(201).json(allInternships)
-        
-     } catch (error) {
-        return res.status(500).json({message:error})
-     }
-})
+router.get("/", async (req, res) => {
+  try {
+    const allInternships = await Internship.find();
+    return res.status(201).json(allInternships);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
 
-export {router}
+//Getting one Internship
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const singleInternship = await Internship.findById(id);
+    return res.status(200).json(singleInternship);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+
+// Updating The Internships
+router.patch("/:id", async (res, req) => {
+  try {
+    const { id } = req.params;
+    const updateInternship = await Internship.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    return res.status(201).json(updateInternship)
+  } catch (error) {
+
+    return res.status(500).json({message:error})
+  }
+});
+
+//Deleting An Internship
+
+router.delete("/:id",async(res,req)=>{
+    try {
+        const {id}=req.params
+        const deleteInternship=await Internship.findByIdAndDelete(id)
+        return res.status(200).json(deleteInternship)
+        
+    } catch (error) {
+        return res.status(500).json({message:error})
+        
+    }
+})
+export { router }
